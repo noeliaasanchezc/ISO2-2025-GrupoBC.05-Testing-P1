36d29123552e570a11a50eb4eed216f389d03118
CarlosUCLM <carlos.soto@alu.uclm.com>
2025-11-26 12:32:15 +0100

    Implementación definitiva lista para testing

--- src/main/java/MetodoFechas.java
+++ src/main/java/MetodoFechas.java
@@ -1,24 +1,37 @@
-
+import java.util.*;
 public class MetodoFechas{
 
 public static boolean verBisiesto( int año) {
-
+	
 	boolean bisiesto;
 	
 	if (año<0) {
 		throw new IllegalArgumentException("El año no puede ser negativo");
 	}
 	
-	if (año%4 !=0){
-		bisiesto= false;
-	} else {
-		if (año%400==0) {
-			bisiesto= true;
-		}else {
-			bisiesto= false;
-		}
-	}
+	 if (año % 4 != 0) {
+	        bisiesto = false;
+	    } else if (año % 100 != 0) { 
+	        bisiesto = true;
+	    } else if (año % 400 == 0) { 
+	        bisiesto = true;
+	    } else {
+	        bisiesto = false;
+	    }
 	return bisiesto;
 }
-
+public static void main(String[] args) {
+	Scanner TECLADO= new Scanner(System.in);
+	System.out.println("meta usted el año en el que estamos");
+	int fecha = TECLADO.nextInt();
+	
+	boolean bisiesto= verBisiesto(fecha);
+	
+	if (bisiesto==true) {
+		System.out.println("El año es bisiesto");
+		
+	}else {
+		System.out.println("El año no es bisiesto");
+	}
+}
 }
