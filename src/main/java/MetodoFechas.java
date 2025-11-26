53ddedea8a7649dffd4b70cdfe81d0f509157e29
CarlosUCLM <carlos.soto@alu.uclm.com>
2025-11-26 11:00:20 +0100

    Creacion método MetodoFechas

--- src/main/java/MetodoFechas.java
+++ src/main/java/MetodoFechas.java
@@ -1,4 +1,13 @@
 
-public class MetodoFechas {
+public static boolean MetodoFechas ( int anio) {
 
+	boolean bisiesto = false;
+	
+	if (anio<0) {
+		throw new IllegalArgumentException("El año no puede ser negativo");
+	}
+	
+	if (anio%4 !=0){
+		bisiesto= false;
+	}
 }
